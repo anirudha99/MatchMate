e4f6814c34f3238f639c40a71df19e9c7efbbef3
Anirudha SM <anirudha.sm@ymedialabs.com>
2024-12-15 16:04:35 +0530

    updated the navigation bar

--- MatchMate/MatchMate/MatchMateApp.swift
+++ MatchMate/MatchMate/MatchMateApp.swift
@@ -11,6 +11,22 @@ import SwiftUI
 struct MatchMateApp: App {
     let persistenceController = PersistenceController.shared
 
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        
+        let pinkColor = UIColor(red: 1.0, green: 0.75, blue: 0.8, alpha: 1)
+        appearance.backgroundColor = pinkColor
+        
+        appearance.titleTextAttributes = [
+            .foregroundColor: UIColor.black,
+            .font: UIFont.boldSystemFont(ofSize: 24)
+        ]
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
